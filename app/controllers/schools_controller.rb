f83a1f96821b47c7f1439fe8f5fd551d88eeafd8
Matt Uyehara <laieboy@gmail.com>
2019-09-12 20:34:03 -0600

    Finished on the testing

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -1,20 +1,50 @@
 class SchoolsController < ApplicationController
+  before_action :set_school, only: [:show, :edit, :update, :destroy]
   # GET /schools
+  def index
+    @schools = School.all
+  end
 
-  # GET /schools/1
+  def show
+  end
 
-  # GET /schools/new
+  def new
+    @school = School.new
+  end
 
-  # GET /schools/edit
+  def create
+    @school = School.new(school_params)
+    if @school.save
+      redirect_to @school
+    else
+      render :new
+    end
+  end
 
-  # POST /schools
+  def edit
+  end
 
-  # DELETE /schools
+  def update
+    if @school.update(school_params)
+      redirect_to @school
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @school.destroy
+    redirect_to schools_path
+  end
 
   private
-
     # Use callbacks to share common setup or constraints between actions.
+    def set_school
+      @school = School.find(params[:id])
+    end
 
+    def school_params
+      params.require(:school).permit(:name, :address, :principal, :capacity, :private_school)
     # Only allow a trusted parameter "white list" through.
-
-end
+    end
+ end
--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -1,5 +1,55 @@
 require 'rails_helper'
 
-RSpec.describe Student, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe School, type: :model do
+  
+  describe 'attributes' do
+    it 'has a name' do
+      name = 'BurgerU'
+      student = Student.create(name: name)
+      expect(student.name).to eq(name)
+    end
+
+    it 'has a gpa' do
+      gpa = 4.0
+      student = Student.create(gpa: gpa)
+      expect(student.gpa).to eq(gpa)
+    end
+
+    it 'has a student number' do
+      student_number = 1234
+      student = Student.create(student_number: student_number)
+      expect(student.student_number).to eq(student_number)
+    end
+
+    it 'has a student id' do
+      student_id = 123
+      student = Student.create(student_id: student_id)
+      expect(student.student_id).to eq(student_id)
+    end
+
+  describe "vaildations" do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :gpa }
+    it { should validate_presence_of :student_number }
+    it { should validate_presence_of :school_id }
+  end
+  
+  describe "association" do
+    it { should belongs_to(:schools) }
+    it { should belongs_to(:schools).dependent(:destroy) }
+  end
+
+  describe "uniqueness" do
+    Student.create!(name: 'foo', gpa: 4.0, student_number: 1234, school_id: 123)
+    subject { Student.new(name: 'foo', gpa: 4.0, student_number: 1234, school_id: 123) }
+    it { should validate_uniqueness_of(:name) }
+  end
+
+  describe "numericality" do
+    before(:each) do
+      @school = School.create(name: 'foo', gpa: 4.0, student_number: 12345, school_id: 123)
+    end
+    
+     it { should validate_numericality_of(:capacity) }
+  end 
 end
